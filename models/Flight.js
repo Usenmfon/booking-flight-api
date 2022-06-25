c69fadb3b21bad92104c54f4e9d17e6a5b9759d1
Usenmfon <usenmfonuko@gmail.com>
2022-06-25 07:58:45 +0100

    added controllers, routes, and readme

--- models/Flight.js
+++ models/Flight.js
@@ -1,14 +1,1 @@
-exports.exampleModel = [
-    {
-        title: "flight to canada",
-        time: "1pm",
-        price: 26000,
-        date: "26-06-2022"
-    },
-    {
-        title: "flight to florida",
-        time: "5pm",
-        price: 10000,
-        date: "30-06-2022"
-    },
-];
+exports.exampleModel = [];
